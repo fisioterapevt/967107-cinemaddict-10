e26f6cd8b336826d001f34170bd74f4fb839647b
Tim Bright <timbright@MacBook-Pro-Tim.local>
2019-12-11 11:45:18 +0600

    change of path

--- src/components/film-details.js
+++ src/components/film-details.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils/utils';
+import {createElement} from '../utils/elements';
 
 const createFilmDetailsPopupTemplate = (film) => {
   const {name, ageRestricted, poster, director, writer, actor, duration, country, genre, rating, description, countComments} = film;
